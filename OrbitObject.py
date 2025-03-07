3ba800fe818d1e2d987d0ea0f2ef272344af63b7
Isaac Charcos <81037035+isaaccharcos@users.noreply.github.com>
2025-03-07 13:42:12 -0700

    Add comments to OrbitObject.py

--- OrbitObject.py
+++ OrbitObject.py
@@ -6,7 +6,7 @@ class OrbitObject:
         self.reset(a, e, mu)
 
     def reset(self, a, e, mu):
-        self.a = a  # Semi-major axis
+        self.a = a  # Semimajor axis
         self.e = e  # Eccentricity
         self.mu = mu # Gravitational parameter
         self.T = 2 * np.pi * np.sqrt(self.a**3 / mu)  # Orbital period
@@ -17,18 +17,30 @@ class OrbitObject:
         return OrbitObject(self.a, self.e, self.mu)
         
     def get_radius(self, nu):
+        """
+        Return radius at given true anomaly
+        """
         r = self.a * (1 - self.e**2) / (1 + self.e * np.cos(nu))
         return r
 
     def get_speed(self, nu):
+        """
+        Return speed at given true anomaly
+        """
         r = self.get_radius(nu)
         v = np.sqrt(self.mu * (2 / r - 1 / self.a))  # Vis-viva equation
         return v
 
     def get_period(self):
+        """
+        Return orbital period
+        """
         return self.T
 
     def get_true_anomaly_after_time(self, t, nu_0=0):
+        """
+        Return true anomaly after time elapsed starting initial true anomaly
+        """
         # Calculate initial eccentric anomaly
         E_0 = 2 * np.arctan(np.sqrt((1 - self.e) / (1 + self.e)) * np.tan(nu_0 / 2))
         
@@ -50,20 +62,26 @@ class OrbitObject:
         return nu_t
 
     def get_position_in_cartesian(self, nu):
+        """
+        Return position in cartesian (for calculating distances)
+        """
         r = self.get_radius(nu)
         x = r * np.cos(nu)
         y = r * np.sin(nu)
         return np.array([x, y])
 
     def apply_delta_v(self, delta_v, nu):
-        # Get the current position and velocity at the given true anomaly
+        """
+        Apply delta v at a given true anomaly and update member orbital elements
+        """
+        # Get current position and velocity at given true anomaly
         r = self.get_radius(nu)
         v_initial = self.get_speed(nu)
 
-        # New velocity after applying delta-v
+        # New velocity after applying delta v
         v_new = v_initial + delta_v
 
-        # Calculate new semi-major axis
+        # Calculate new semimajor axis
         a_new = 1 / (2 / r - v_new**2 / self.mu)
 
         # Calculate new eccentricity
